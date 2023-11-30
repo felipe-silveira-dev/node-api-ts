dc4f5e3479e6ea56da2f2e99f603999f08fedadd
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-11-30 20:26:12 -0300

    adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import routes from './routes/routes';
+import cors from 'cors'
 
 const app = express();
-
+app.use(cors())
 app.use(routes);
 
 app.listen(3000, () => {
